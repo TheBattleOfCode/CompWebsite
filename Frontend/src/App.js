60153d2e8a2ef9f368b224b5d91e4bbfc0bb73ef
Theeother <44754872+Theeother@users.noreply.github.com>
2022-04-01 14:01:40 +0100

    Creating problem of type "Number Gen" done

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -22,9 +22,9 @@ import Contest from "./components/Contest";
 import Standings from "./components/Standings";
 
 const App = () => {
-  const [showModeratorBoard, setShowModeratorBoard] = useState(false);
-  const [showAdminBoard, setShowAdminBoard] = useState(false);
-  const [currentUser, setCurrentUser] = useState(undefined);
+  const [ showModeratorBoard, setShowModeratorBoard ] = useState(false);
+  const [ showAdminBoard, setShowAdminBoard ] = useState(false);
+  const [ currentUser, setCurrentUser ] = useState(undefined);
 
   useEffect(() => {
     const user = AuthService.getCurrentUser();
@@ -58,12 +58,12 @@ const App = () => {
           CompWeb
         </Link>
         <div className="navbar-nav mr-auto">
-        <li className="nav-item">
+          <li className="nav-item">
             <Link to={"/home"} className="nav-link">
               Home
             </Link>
           </li>
-          
+
           {showModeratorBoard && (
             <li className="nav-item">
               <Link to={"/mod"} className="nav-link">
@@ -80,6 +80,14 @@ const App = () => {
             </li>
           )}
 
+          {showAdminBoard && (
+            <li className="nav-item">
+              <Link to={"/createProb"} className="nav-link">
+                Create Problem
+              </Link>
+            </li>
+          )}
+
           {currentUser && (
             <li className="nav-item">
               <Link to={"/user"} className="nav-link">
@@ -87,12 +95,12 @@ const App = () => {
               </Link>
             </li>
           )}
-          
+
           <li className="nav-item">
             <Link to={"/contest"} className="nav-link">
               Contests
             </Link>
-          
+
           </li>
           <li className="nav-item">
             <Link to={"/standings"} className="nav-link">
@@ -133,7 +141,7 @@ const App = () => {
 
       <div className="container mt-3">
         <Switch>
-          <Route exact path={["/", "/home"]} component={Home} />
+          <Route exact path={[ "/", "/home" ]} component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/profile" component={Profile} />
--- Frontend/src/components/CreateProblem.js
+++ Frontend/src/components/CreateProblem.js
@@ -1,128 +1,134 @@
-import React,{useState} from "react";
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
+import React, { useState } from "react";
+import probService from "../services/prob.service";
+
+
 
 const CreateProblem = () => {
-  
-    
-    const [selectValue,setSelectValue]=useState("");
-    
-    const [probTitle,setProbTitle]=useState("");
-    const [probDesc,setProbDesc]=useState("");
-    const [probScore,setProbScore]=useState("");
-    const [probFile,setProbFile]=useState("");
-    const [probInput,setProbInput]=useState([]);
-    const [probOutput,setProbOutput]=useState(0);
-  
 
 
-    const handleChange = (e) => {
-      console.log("done");
-    }
-    const submitNumberGen=()=>{
-      console.log("done");
-      eval(probFile);
-      console.log(probInput);
-      console.log(probOutput);
-    }
+  const [ selectValue, setSelectValue ] = useState("");
 
-    const checkSubmit=()=>{
-      console.log("checking");
-      console.log(probInput);
-      console.log(probOutput);
-    }
+  const [ probTitle, setProbTitle ] = useState("");
+  const [ probDesc, setProbDesc ] = useState("");
+  const [ probScore, setProbScore ] = useState("");
+  const [ probFile, setProbFile ] = useState("");
 
-    return (
+
+
+
+
+  const submitNumberGen = () => {
+    console.log("To check input");
+
+    probService.SaveProb(
+      probTitle,
+      probDesc,
+      probScore,
+      probFile,
+      "NumberGen").then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+
+    console.log("done");
+
+
+  }
+
+
+
+  return (
+
+    <div>
+      <h1>Create Problem</h1>
+      <h2>Select the problem style :</h2>
 
       <div>
-          <h1>Create Problem</h1>
-          <h2>Select the problem style :</h2>
-          
+        <select id="dropdown" onChange={(e) => setSelectValue(e.target.value)}>
+          <option value="N/A">Choose a problem style</option>
+          <option value="Number gen">Number gen</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+      </div>
+      <h3>{selectValue}</h3>
+
+      <div className="form-group">
+        <label htmlFor="Title">Title</label>
+        <input
+          type="text"
+          className="form-control"
+          name="Title"
+          value={probTitle}
+          onChange={(e) => setProbTitle(e.target.value)}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="Description">Description</label>
+        <textarea
+          type="text"
+          className="form-control"
+          name="Description"
+          value={probDesc}
+          onChange={(e) => setProbDesc(e.target.value)}
+        />
+      </div>
+
+
+      <div className="form-group">
+        <label htmlFor="Score">Score</label>
+        <input
+          type="text"
+          className="form-control"
+          name="Score"
+          value={probScore}
+          onChange={(e) => setProbScore(e.target.value)}
+        />
+      </div>
+
+      {selectValue === "Number gen" && <div>
+
+        {
           <div>
-            <select id="dropdown" onChange={(e) => setSelectValue(e.target.value)}>
-              <option value="N/A">Choose a problem style</option>
-              <option value="Number gen">Number gen</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-            </select>
-          </div>
-          <h3>{selectValue}</h3>
-
-          <div className="form-group">
-                <label htmlFor="Title">Title</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="Title"
-                  value={probTitle}
-                  onChange={(e)=>setProbTitle(e.target.value)}
-                />
-          </div>
-
-          <div className="form-group">
-                <label htmlFor="Description">Description</label>
-                <textarea
-                  type="text"
-                  className="form-control"
-                  name="Description"
-                  value={probDesc}
-                  onChange={(e)=>setProbDesc(e.target.value)}
-                />
-          </div>
 
 
-          <div className="form-group">
-                <label htmlFor="Score">Score</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="Score"
-                  value={probScore}
-                  onChange={(e)=>setProbScore(e.target.value)}
-                />
-          </div>
-
-          {selectValue==="Number gen" && <div>
-          
-          {
-            <div>
-              
-              
 
 
-              <div className="form-group">
-                <label htmlFor="File">File</label>
-                <textarea
-                  type="text"
-                  className="form-control"
-                  name="File"
-                  value={probFile}
-                  onChange={(e)=>setProbFile(e.target.value)}
-                />
-              </div>
-
-
-              
-
-              <div className="form-group">
-                <button className="btn btn-primary btn-block" onClick={submitNumberGen}>Submit</button>
-              </div>
-              <div className="form-group">
-                <button className="btn btn-primary btn-block" onClick={checkSubmit}>check</button>
-              </div>
+            <div className="form-group">
+              <label htmlFor="File">File</label>
+              <textarea
+                type="text"
+                className="form-control"
+                name="File"
+                value={probFile}
+                onChange={(e) => setProbFile(e.target.value)}
+              />
             </div>
-          }
 
-          
-            </div>}
 
-            
-    
+
+
+            <div className="form-group">
+              <button className="btn btn-primary btn-block" onClick={submitNumberGen}>Submit</button>
+            </div>
+          </div>
+        }
+
+
+      </div>}
+
+
+
     </div>
 
-        
-    );
+
+  );
 }
 
 export default CreateProblem;
