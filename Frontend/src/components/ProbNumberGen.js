93e0525b28a90f0fea3266d471c59c5972229bf0
Theeother <44754872+Theeother@users.noreply.github.com>
2022-04-22 02:24:12 +0100

    feat: Input generation done

--- Frontend/src/components/ProbNumberGen.js
+++ Frontend/src/components/ProbNumberGen.js
@@ -2,28 +2,76 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import authService from "../services/auth.service";
 import genProblemService from "../services/gen-problem.service";
+import { createHash } from "crypto";
 import probService from "../services/prob.service";
 
 function ProbNumberGen() {
-    const currentUser = authService.getCurrentUser();
-    // getting the problem
-
-    const getAnswer = async () => {
-        const currentProblem = probService.GetProb(id);
-        const data = await currentProblem;
-        setProb(data.data);
-    };
-
     let { id } = useParams();
     const [ prob, setProb ] = React.useState([]);
+    const [ inOut, setInOut ] = React.useState([]);
+    const [ generatedInOut, setGeneratedInOut ] = React.useState([]);
+    const currentUser = authService.getCurrentUser();
+
+    function hash(input) {
+        return createHash('sha256').update(id + input + currentUser.id).digest('base64');
+    }
+
+
+    function generateInOut(code) {
+        eval(code);
+        //console.log(generatedInOutFromEval);
+        //console.log(generatedInOut);
+    }
+
+    const getData = async () => {
+        // getting the problem
+        const currentProblem = probService.GetProb(id);
+        const data = await currentProblem;
+        console.log(data);
+        setProb(data.data);
+
+        // getting the Input and Output
+        const inOut = genProblemService.GetGenProb(currentUser.id, id);
+        const dataInOut = await inOut;
+        console.log(dataInOut);
+
+        // if there's no data, then we need to generate it
+        if (!dataInOut.data) {
+            generateInOut(data.data.inOutContentGen);
+
+        }
+        else {
+            setInOut({ "input": dataInOut.data.genInput, "output": dataInOut.data.genOutput });
+        }
+    };
+
+    React.useEffect(() => {
+        if (generatedInOut.output) {
+            console.log(generatedInOut);
+            setInOut({ "input": generatedInOut.input, "output": generatedInOut.output });
+            let encrypted = hash(generatedInOut.output);
+            console.log(encrypted);
+            genProblemService.SaveGenProb(
+                generatedInOut.input.toString(),
+                encrypted,
+                currentUser.id,
+                id).then(
+                    (response) => {
+                        console.log(response);
+                    },
+                    (error) => {
+                        console.log(error);
+                    }
+                );;
+                
+        }
+    }, [ generatedInOut ]);
 
 
     React.useEffect(() => {
-        getAnswer();
-
-        
+        getData();
     }, []);
-    console.log(prob);
+
 
 
     return (
@@ -34,34 +82,34 @@ function ProbNumberGen() {
             <div className="row">
 
                 <div className="col-9 align-self-start">
-                    <h1 className="h1 text-center">{prob.title +" ("+prob.score+" points)"}</h1>
+                    <h1 className="h1 text-center">{prob.title + " (" + prob.score + " points)"}</h1>
                     <h3 className="h3">Description</h3>
                     <p className="fs-4 p-sm-3 text-sm-start">{prob.description}</p>
 
                 </div>
 
-                
+
             </div>
 
 
             {!!currentUser && <div className="row justify-content-start" id="aa">
                 <div className="col-9">
                     <h4 className="h4">Click to get your input</h4>
-                    <button onClick={() => navigator.clipboard.writeText("copied Text")}>Copy</button>
+                    <button onClick={() => navigator.clipboard.writeText(inOut.input)}>Copy</button>
 
                     <div>
                         <br></br>
-                        <input type="text" name="submission"  placeholder="Your answer here" />
+                        <input type="text" name="submission" placeholder="Your answer here" />
                         <button type="submit" name="submit">submit (to do)</button>
                     </div>
                 </div>
 
-                
+
 
             </div>}
             {!currentUser &&
-                    <p>To get an Input and get solving, <a href="login.php">login</a> or <a href="signup.php">signup</a>.</p>
-}
+                <p>To get an Input and get solving, <a href="login.php">login</a> or <a href="signup.php">signup</a>.</p>
+            }
 
             <footer className="card-footer">
                 <p>Author: Foulen ben foulen</p>
--- Frontend/src/services/gen-problem.service.js
+++ Frontend/src/services/gen-problem.service.js
@@ -2,18 +2,18 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/generatedInOut/";
 
-const SaveGenProb = (genInput, genOutput, answered, userId, problemId) => {
+const SaveGenProb = (genInput, genOutput, userId, problemId) => {
   return axios.post(API_URL+"add", { 
     genInput,
     genOutput,
-    answered,
     userId,
     problemId
   });
 }
-
+ 
 const GetGenProb = (idUser, idGenProb) => {
-    return axios.get(API_URL+"getone/"+idUser+'/'+idGenProb);
+    console.log(idUser, idGenProb);
+    return axios.get(API_URL+"getOne/"+idUser+'/'+idGenProb);
 }
 
 export default {
