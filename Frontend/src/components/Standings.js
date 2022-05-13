e26e29e9ec65e9ae3a63975dd61c2e895643530f
housssem1999 <62448561+housssem1999@users.noreply.github.com>
2022-05-13 02:52:57 +0100

    Frontend: Data from DB for Standings page

--- Frontend/src/components/Standings.js
+++ Frontend/src/components/Standings.js
@@ -1,104 +1,47 @@
-import React from "react";
-
-const Standings = () =>{
-    return(
-        <table class="table">
-    <thead class="thead-dark">
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">TEAM_Name</th>
-        <th scope="col">MP</th>
-        <th scope="col">MW</th>
-        <th scope="col">ML</th>
-        <th scope="col">Score</th>
-        <th scope="col">Bonus</th>
-        <th scope="col">Last 5 Rounds</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Team_1</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>00000</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Team_2</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>00000</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Team_3</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>00000</td>
-      </tr>
-      <tr>
-        <th scope="row">4</th>
-        <td>Team_4</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>00000</td>
-      </tr>
-      <tr>
-        <th scope="row">5</th>
-        <td>Team_5</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>00000</td>
-      </tr>
-      <tr>
-        <th scope="row">6</th>
-        <td>Team_6</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>00000</td>
-      </tr>
-      <tr>
-        <th scope="row">7</th>
-        <td>Team_7</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>00000</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Team_8</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>00000</td>
-      </tr>
-    </tbody>
-  </table>
+import React, { useEffect, useState } from "react";
+import userService from "../services/user.service";
   
-    )
-}
+  const Standings = () =>{
+    const  [users, setUsers] = useState([]);
+    const getUser = async() => {
+      const data = await userService.getUsers();     
+      setUsers( data.data.sort( (a, b) => b.indivScore - a.indivScore ) );     
+      users.sort( (a, b) => b.indivScore - a.indivScore );        
+    }      
+    
+    useEffect(() => {     
+      getUser();    
+    } , []);     
+    
+    return(         
+    <table class="table">     
+      <thead class="thead-dark">      
+       <tr>        
+        <th scope="col">#</th>        
+        <th scope="col">TEAM_Name</th>         
+        <th scope="col">MP</th>         
+        <th scope="col">MW</th>         
+        <th scope="col">ML</th>         
+        <th scope="col">Score</th>         
+        <th scope="col">Bonus</th>         
+        <th scope="col">Last 5 Rounds</th>       
+      </tr>     </thead>     
+      <tbody>          
+          {users.map( (user, index) => {        
+              return(         
+                  <tr key={index}>             
+                    <td >{user.teamName}</td>               
+                    <td >{user.lastName}</td> 
+                    <td >{user.firstName}</td>
+                    <td >{user.firstName}</td>             
+                    <td >{user.phone}</td> 
+                    <td>{user.teamScore}</td>
+                    <td>{user.indivScore}</td>
+                    <td>0.0</td>          
+                  </tr>         
+                )}        
+              )}     
+              </tbody> 
+            </table>        
+          ) } 
 export default Standings;
