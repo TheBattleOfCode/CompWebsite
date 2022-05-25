57cf9919a5497adfcdefea24aff40b83c455c579
Theeother <44754872+Theeother@users.noreply.github.com>
2022-05-25 02:01:03 +0100

    feat: Qna style problems fix: some refractoring

---
+++ QnaFileTemp.js
@@ -0,0 +1,9 @@
+function generateInput(){
+    return "";
+}
+
+function generateOutput(input){
+    return "";
+}
+const input=generateInput();
+setGeneratedInOut({"input":input,"output":generateOutput(input)});
