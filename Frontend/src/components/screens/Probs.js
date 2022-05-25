5c98def57794f908abb1c1ff448ac04ac9d99b13
NeejiMed <mohamedneji2410@gmail.com>
2022-05-25 20:15:34 +0100

    fix some home colors

--- Frontend/src/components/screens/Probs.js
+++ Frontend/src/components/screens/Probs.js
@@ -17,7 +17,7 @@ export default function Prob({ prob }) {
 
     useEffect(() => {
         if (inOut.data) {
-            setStateProb(inOut.data.answered? "success" : "secondary");
+            setStateProb(inOut.data.answered? "bg-success" : "table-active");
         }
     }, [inOut]);
     useEffect(() => {
@@ -27,12 +27,12 @@ export default function Prob({ prob }) {
 
 
     return (
-        <tr className={"bg-"+ stateProb +" rounded"}>
+        <tr className={stateProb +" rounded"}>
             <td>{prob.title}</td>
             <td>{prob.type} </td>
             <td>
-                <button className="btn btn-outline-primary" >
-                    <a href={'/ProbNumberGen/' + prob._id}>
+                <button className={stateProb != "bg-success" ?"btn btn-outline-primary":"btn btn-outline-dark"} >
+                    <a href={'/ProbNumberGen/' + prob._id} style={{color:stateProb == "bg-success"?"black":"blue"}}>
                         Enter
                     </a>
                 </button>
